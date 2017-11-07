ae96169ad881b6878a90777e0b73e1899e392740
Grant Zargaryan <grantzarg@gmail.com>
2017-11-07 14:38:37 +0300

    added animation

---
+++ js/main.js
@@ -0,0 +1,48 @@
+ $('.popup-1').magnificPopup({
+ type: 'image',
+ zoom: {
+     enabled: true,
+     duration: 300
+ }
+ });
+
+ $('.popup-2').magnificPopup({
+ type: 'image',
+ zoom: {
+     enabled: true,
+     duration: 300
+ }
+ });
+
+ $('.popup-3').magnificPopup({
+ type: 'image',
+ zoom: {
+     enabled: true,
+     duration: 300
+ }
+ });
+
+ $('.popup-4').magnificPopup({
+ type: 'image',
+ zoom: {
+     enabled: true,
+     duration: 300
+ }
+ });
+
+ $('.popup-5').magnificPopup({
+ type: 'image',
+ zoom: {
+     enabled: true,
+     duration: 300
+ }
+ });
+
+ $('.popup-6').magnificPopup({
+ type: 'image',
+ zoom: {
+     enabled: true,
+     duration: 300
+ }
+ });
+
